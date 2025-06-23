86a6c0d106bb1d94f9356381dffb6204589d2f38
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-06-23 11:43:30 +0200

    general adjustments in navbar and footer

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -7,3 +7,13 @@ hamMenu.addEventListener('click', (e)=>{
     hamMenu.classList.toggle('ph-x')
     navbar.classList.toggle('active')
 })
+
+const links = document.querySelectorAll('#navbar-mobile a')
+console.log(links)
+
+links.forEach(link => {
+    link.addEventListener('click', ()=>{
+        hamMenu.classList.toggle('ph-list');
+        navbar.classList.toggle('active');
+    })
+})
